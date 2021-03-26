d21505e123133712e6c5ba487fc11ae1b0e62fe1
stephm <mansell.steph@gmail.com>
2021-03-26 23:43:41 +0000

    First commit

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+  return (
+    <div className="navbar-container">
+      <div className="navbar">
+        <br></br>
+        <br></br>
+        <br></br>
+      </div>
+      <img
+        src="https://www.pngkit.com/png/full/19-190666_pokeball-graphic-by-maratuna-on-deviantart-banner-free.png"
+        className="pokedex_img"
+      />
+    </div>
+  );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,140 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { getAllPokemon, getPokemon } from "./Services/pokemon.js";
+import Card from "./components/Card";
+import Navbar from "./Navbar";
+
+function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+  const initialUrl = "https://pokeapi.co/api/v2/pokemon?limit=151";
+
+  const [searchPoke, setSearchPoke] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const [searchType, setSearchType] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialUrl);
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      let pokemon = await loadingPokemon(response.results);
+      console.log(pokemon);
+      setLoading(false);
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const results = pokemonData.filter(
+      (poke) =>
+        (poke.name.includes(searchPoke.toLowerCase()) ||
+          poke.id.toString().includes(searchPoke)) &&
+        poke.types[0].type.name.includes(searchType.toLowerCase())
+    );
+
+    setSearchResults(results);
+  }, [pokemonData, searchPoke, searchType]);
+
+  const next = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  };
+
+  const prev = async () => {
+    if (!prevUrl) return;
+    setLoading(true);
+    let data = await getAllPokemon(prevUrl);
+    await loadingPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  };
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map(async (pokemon) => {
+        let pokemonRecord = await getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  const handleChange = (event) => {
+    setSearchPoke(event.target.value);
+  };
+
+  const typeChange = (event) => {
+    setSearchType(event.target.value);
+  };
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <Navbar />
+          <div className="btn">
+            <button onClick={prev}>Prev</button>
+            <button onClick={next}>Next</button>
+          </div>
+          <div className="search-filter">
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchPoke}
+              onChange={handleChange}
+              className="search-bar"
+            />
+            <select type="select" onChange={typeChange} className="types">
+              <option value="">Types:</option>
+              <option value="">All</option>
+              <option value="bug">Bug</option>
+              <option value="dragon">Dragon</option>
+              <option value="fairy">Fairy</option>
+              <option value="fire">Fire</option>
+              <option value="ghost">Ghost</option>
+              <option value="ground">Ground</option>
+              <option value="normal">Normal</option>
+              <option value="psychic">Psychic</option>
+              <option value="steel">Steel</option>
+              <option value="dark">Dark</option>
+              <option value="electric">Electric</option>
+              <option value="fighting">Fighting</option>
+              <option value="flying">Flying</option>
+              <option value="grass">Grass</option>
+              <option value="ice">Ice</option>
+              <option value="poison">Poison</option>
+              <option value="rock">Rock</option>
+              <option value="water">Water</option>
+            </select>
+          </div>
+
+          <div className="container">
+            {searchResults.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
